590a733af7b83154ae14025ffa88adc2e3058fea
Graham Batty <graham@stormbrew.ca>
2013-06-30 15:10:01 -0600

    Generate correct row and column information for sublime.

--- test_find_gaps.py
+++ test_find_gaps.py
@@ -1,6 +1,6 @@
 # these are actually offsets into the coordinate mappings.
-Horizontal = 0
-Vertical = 1
+Horizontal = 1
+Vertical = 2
 
 class DisplayCell(object):
 	def __init__(self, layout, coords):
@@ -38,6 +38,8 @@ class DisplayGroup(DisplayCell):
 		for pane in panes:
 			pane.set_parent(self)
 
+		self.orientation = self.find_orientation(self.panes)
+
 		left = min(pane.left for pane in panes)
 		top = min(pane.top for pane in panes)
 		right = max(pane.right for pane in panes)
@@ -46,31 +48,53 @@ class DisplayGroup(DisplayCell):
 		super(DisplayGroup, self).__init__(layout, (left,top,right,bottom))
 
 	def __repr__(self):
-		return "%s: [%s]" % (super(DisplayGroup, self).__repr__(),
+		return "%s: %s[%s]" % (super(DisplayGroup, self).__repr__(),
+			"Vertical" if self.orientation == Vertical else "Horizontal",
 			", ".join(pane.__repr__() for pane in self.panes))
+
+	@staticmethod
+	def find_orientation(panes):
+		last_orientation = None
+		last = panes[0]
+		for pane in panes[1:]:
+			if pane.left == last.left and pane.right == last.right:
+				orientation = Vertical
+			elif pane.top == last.top and pane.bottom == last.bottom:
+				orientation = Horizontal
+			else:
+				raise "Unmatched panes in the same group!"
+
+			if last_orientation:
+				if orientation != last_orientation:
+					raise "Unexpected flip in orientation!"
+			else:
+				last_orientation = orientation
+
+		return last_orientation
+
 
 class Layout(object):
 	def __init__(self, layout):
-		self.cells = [DisplayPane(self, pane, idx) for idx, pane in enumerate(layout['cells'])]
-		self.rows = layout['rows']
-		self.cols = layout['cols']
-		self.groups = self.extract_groups(self.cells)
+		self.cells = [DisplayPane(self, self._deref_splits(pane, layout['rows'], layout['cols']), idx) for idx, pane in enumerate(layout['cells'])]
+
+		self.groups = self._extract_groups(self.cells)
 
 	def __repr__(self):
 		return "%s: %s" % (self.__class__.__name__, self.groups.__repr__())
 
-	def make_sublime_layout(self):
-		return {
-			'cells': [[cell.left, cell.top, cell.right, cell.bottom] for cell in self.cells],
-			'rows': self.rows,
-			'cols': self.cols,
-		}
+	@staticmethod
+	def _deref_splits(raw_pane, rows, cols):
+		return (
+			cols[raw_pane[0]],
+			rows[raw_pane[1]],
+			cols[raw_pane[2]],
+			rows[raw_pane[3]],
+		)
 
-	def search_groups(self, groups, search, remain):
+	def _search_groups(self, groups, search, remain):
 		newremain = []
 		group = [search]
 		for cell in remain:
-			# TODO: Error on flip. Shouldn't ever happen.
 			if search.by(Horizontal) == cell.by(Horizontal):
 				group.append(cell)
 			elif search.by(Vertical) == cell.by(Vertical):
@@ -83,20 +107,84 @@ class Layout(object):
 			groups.append(search)
 
 		if len(newremain) > 0:
-			return self.search_groups(groups, newremain[0], newremain[1:])
+			return self._search_groups(groups, newremain[0], newremain[1:])
 		else:
 			return groups
 
-	def extract_groups(self, cells):
+	def _extract_groups(self, cells):
 		groups = cells
 		while 1:
-			newgroups = self.search_groups([], groups[0], groups[1:])
+			newgroups = self._search_groups([], groups[0], groups[1:])
 			if len(newgroups) == 1:
 				return newgroups[0]
 			elif len(newgroups) == len(groups):
 				raise "Grouping stalled! At: %s" % (groups,)
 
 			groups = newgroups
+
+	@staticmethod
+	def _make_splitid(cell, value, split_list):
+		cur = cell
+		while cur != None:
+			group = cur.parent
+			if (group, value) in split_list:
+				return split_list.index((group, value))
+			cur = group
+
+		split_list.append((cell.parent, value))
+		return len(split_list) - 1
+
+	@staticmethod
+	def _depth_walk(cell):
+		nodes = [cell]
+		while nodes:
+			cell = nodes.pop()
+			yield cell
+			if isinstance(cell, DisplayGroup):
+				nodes = cell.panes + nodes
+
+	def make_sublime_layout(self):
+		if len(self.cells) == 1:
+			return {'cells': [[0,0,1,1]], 'rows': [0.0,1.0], 'cols': [0.0,1.0]}
+
+		output_cells = []
+		output_rows = []
+		output_cols = []
+
+		# Generate output_rows and output_cols first,
+		# then sort them, then point the cells at them.
+		# This is so the split bars come out in a sane
+		# order that won't blow up sublime
+
+		# We do a depth-first scan on the first pass because
+		# we want panes to share edges with other
+		# objects in their parent group(s).
+		#import pdb; pdb.set_trace()
+		for cell in self._depth_walk(self.groups):
+			self._make_splitid(cell, cell.left, output_cols)
+			self._make_splitid(cell, cell.top, output_rows)
+			self._make_splitid(cell, cell.right, output_cols)
+			self._make_splitid(cell, cell.bottom, output_rows)
+
+		# put them in order
+		output_cols[:] = sorted(output_cols, key=lambda split: split[1])
+		output_rows[:] = sorted(output_rows, key=lambda split: split[1])
+
+		# now build the cells list in the original cell order, which
+		# has to be maintained.
+		for cell in self.cells:
+			output_cells.append([
+				self._make_splitid(cell, cell.left, output_cols),
+				self._make_splitid(cell, cell.top, output_rows),
+				self._make_splitid(cell, cell.right, output_cols),
+				self._make_splitid(cell, cell.bottom, output_rows),
+			])
+
+		return {
+			'cells': output_cells,
+			'rows': [row[1] for row in output_rows],
+			'cols': [col[1] for col in output_cols],
+		}
 
 
 #layout = {'cells': [[3, 0, 5, 1], [3, 1, 4, 2], [4, 1, 5, 2], [3, 2, 5, 4], [0, 3, 2, 4], [1, 0, 3, 3]], 'cols': [0.0, 0.25, 0.37291831879460746, 0.5, 0.75, 1.0], 'rows': [0.0, 0.25, 0.5, 0.6875706214689266, 1.0]}
@@ -105,13 +193,14 @@ layout = {
 	'cells': [
 		[0, 0, 2, 1],
 		[2, 0, 4, 2],
+		[4, 0, 5, 2],
 		[0, 1, 2, 2],
 
 		[0, 2, 1, 3],
 		[1, 2, 3, 3],
-		[3, 2, 4, 3],
+		[3, 2, 5, 3],
 	],
-	'cols': [0.0, 0.35, 0.5, 0.8, 1.0],
+	'cols': [0.0, 0.35, 0.5, 0.8, 0.8, 1.0],
 	'rows': [0.0, 0.25, 0.5, 1.0],
 }
 
