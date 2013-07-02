2836622051612a4a1e5da46a300329a67da07e95
Graham Batty <graham@stormbrew.ca>
2013-07-02 14:13:11 -0600

    Made it focus the new pane and added more modes.
    * Ctrl+space, n, [-\]: open a new file in the new pane.
    * Ctrl+space, c, [-\]: copy current view into new pane.
    
    Also reorganized the way commands are called to be more general.

--- submux.py
+++ submux.py
@@ -5,29 +5,37 @@ class SubmuxCommand(sublime_plugin.WindowCommand):
 	def _layout(self):
 		return sublime_layout.Layout(self.window.get_layout())
 
-	def split_vertical(self):
-		layout = self._layout()
-		layout.split_pane(self.window.active_group(), sublime_layout.Vertical)
-		return layout
+	def split(self, layout, direction, open):
+		direction = getattr(sublime_layout, direction)
+		cur_pane = self.window.active_group()
+		cur_view = self.window.active_view()
+		new_pane = layout.split_pane(cur_pane, direction)
 
-	def split_horizontal(self):
-		layout = self._layout()
-		layout.split_pane(self.window.active_group(), sublime_layout.Horizontal)
-		return layout
+		# if we're going to copy it we need to create a duplicated
+		# view of the currenly active file first. If we're moving it
+		# we need to at least record the currently active view to
+		# 
+		if cur_view and open == 'copy':
+			group, move_index = self.window.get_view_index(cur_view)
+			self.window.run_command("clone_file")
+			cur_view = self.window.active_view()
+			# move it to the left so focus falls back to the original
+			# window (thanks Origami)
+			self.window.set_view_index(cur_view, group, move_index)
 
-	def delete_current_pane(self):
-		layout = self._layout()
+		self.window.set_layout(layout.make_sublime_layout())
+		self.window.focus_group(new_pane)
+		if open == 'new':
+			self.window.new_file()
+		elif cur_view and (open == 'copy' or open == 'move'):
+			self.window.set_view_index(cur_view, new_pane, 0)
+
+	def delete_current_pane(self, layout):
 		layout.delete_pane(self.window.active_group())
 		return layout
 
-class SplitVerticalCommand(SubmuxCommand):
-	def run(self):
-		self.window.set_layout(self.split_vertical().make_sublime_layout())
-
-class SplitHorizontalCommand(SubmuxCommand):
-	def run(self):
-		self.window.set_layout(self.split_horizontal().make_sublime_layout())
-
-class DeleteCurrentPaneCommand(SubmuxCommand):
-	def run(self):
-		self.window.set_layout(self.delete_current_pane().make_sublime_layout())
+	def run(self, **kargs):
+		cmd = kargs['do']
+		del kargs['do']
+		layout = self._layout()
+		return getattr(self, cmd)(layout, **kargs)
